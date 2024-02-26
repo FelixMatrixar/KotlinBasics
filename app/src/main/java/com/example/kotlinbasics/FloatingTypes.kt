ec55ec51623d5b26241f3a8ba6223c1daba08f29
FelixMatrixar <qfelix0112@gmail.com>
2024-02-26 12:19:56 +0700

    file name changes and new added demonstration

---
+++ app/src/main/java/com/example/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinbasics
+
+fun main() {
+    // double
+    var circleRatio = 3.14
+
+    // float
+    // circleRatio = 3.14f
+
+    // float
+    // circleRatio = 3.1415926535F
+
+    // double
+    circleRatio = 3.1415926535897932
+
+    println(circleRatio)
+}
