fc4d15674c50fd30609447520cd9d78a5c1281a9
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-02-26 13:56:49 +0700

    BooleansPractice

---
+++ app/src/main/java/com/example/kotlinbasics/BooleansPractice.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinbasics
+
+fun main () {
+
+    // Step 1: Declaring Boolean Variables
+    var myTrue = true
+    var myFalse = false
+
+    //Step 2: Logical OR Operation
+
+    println(myTrue || myFalse) //output : true
+
+    //Step 3: Logical AND Operation
+    println(myTrue && myFalse) //output : false
+
+    //Step 4: Logical NOT Operation
+    println(!myTrue) //output : false
+}
