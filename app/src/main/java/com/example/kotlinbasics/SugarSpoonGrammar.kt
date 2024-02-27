b57d71e0adb5837821f5edab4c3c552d5a92688b
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-02-27 16:33:51 +0700

    Update SugarSpoonGrammar.kt
    up to Quiz 10

--- app/src/main/java/com/example/kotlinbasics/SugarSpoonGrammar.kt
+++ app/src/main/java/com/example/kotlinbasics/SugarSpoonGrammar.kt
@@ -1,6 +1,27 @@
 package com.example.kotlinbasics
 
 fun main () {
+
+    println("please enter value 1 : ")
+    var num1Str = readln().toInt()
+    println("please enter value 2 : ")
+    var num2Str = readln().toInt()
+
+    var myResult = add(number1 = num1Str, number2 = num2Str)
+
+    println(myResult)
+
+}
+
+fun add (number1: Int, number2: Int) : Int {
+    var result = number1 + number2
+    println("The result of $number1 + $number2 is: $result")
+
+    return result
+}
+
+
+fun askCoffeeDetails () {
     println("Welcome, What's your name")
 
     val name = readln()
@@ -11,9 +32,6 @@ fun main () {
 
 
     makeCoffee(sugarCount= sugarCount, name = name)
-
-
-
 }
 
 fun makeCoffee(sugarCount : Int, name : String){
